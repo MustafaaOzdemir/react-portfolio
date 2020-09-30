43096895b693c3d4195887fd18232159dc679e44
mostafaoglu <mostafaatya077@gmail.com>
2020-09-30 21:53:50 +0300

    Updates

---
+++ precache-manifest.e76cf36b83b488a9c3307306f4e21867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e741385a1f709dd4f47eae8557d87d4",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e8b66163ef01b9b09b92",
+    "url": "/react-portfolio/static/css/main.df72ef5f.chunk.css"
+  },
+  {
+    "revision": "a586e37ce8d338c9f04c",
+    "url": "/react-portfolio/static/js/2.bb1d817f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.bb1d817f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8b66163ef01b9b09b92",
+    "url": "/react-portfolio/static/js/main.000530b9.chunk.js"
+  },
+  {
+    "revision": "ffe35d88d862a4cfcb85",
+    "url": "/react-portfolio/static/js/runtime-main.2184bf1c.js"
+  }
+]);
